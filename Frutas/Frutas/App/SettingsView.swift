e9c50ea124852c73f980147087b413b12ab40722
Erasmo J.F Da Silva <erasmogohiper@gmail.com>
2021-07-09 16:14:26 -0300

    CM - Let’s create an outstanding Settings View - Part 2
    Let’s create an outstanding Settings View - Part 2

--- Frutas/Frutas/App/SettingsView.swift
+++ Frutas/Frutas/App/SettingsView.swift
@@ -41,7 +41,19 @@ struct SettingsView: View {
                     // MARK: SECTION 2
                     
                     
-                    // MARK: SECTION 2
+                    // MARK: SECTION 3
+                    GroupBox(label: SettingsLabelView(labelText: "Application", labelImage: "apps.iphone")) {
+                        
+                      SettingsRowView(name: "Developer", content: "John / Jane")
+                        SettingsRowView(name: "Designer", content: "Robert Petras")
+                        SettingsRowView(name: "Compatibility", content: "iOS 14")
+                        SettingsRowView(name: "Website", linkLabel: "SwiftUI Masterclass", linkDestination: "swiftuimasterclass.com")
+                        SettingsRowView(name: "Twitter",  linkLabel: "@RobertPetras", linkDestination: "twitter.com/robertpetras")
+                        
+                        SettingsRowView(name: "SwiftUI", content: "2.0")
+                        SettingsRowView(name: "Version", content: "1.1.0")
+                        
+                    }// BOX
                     
                 }// STACK
                 .navigationBarTitle(Text("Setting"), displayMode: .large)
